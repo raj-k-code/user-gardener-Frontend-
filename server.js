221daa5399e04332d7a6c0aef11e2b0ed3ee12bd
Narendra Pipliwal <rajkasotiya26@gmail.com>
2022-05-20 22:16:06 +0530

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/user-gardener-Frontend-'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/user-gardener-Frontend-'));
+    res.sendFile(path.join(__dirname + '/dist/user-gardener-Frontend-/index.html'));
 });
 //
 // Start the app by listening on the default Heroku port
